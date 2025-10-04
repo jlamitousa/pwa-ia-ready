8de40dcaa503e6fefa19f6af51120022fb534482
Jean-Luc AMITOUSA MANKOY <jean-luc.amitousa.ext@fillndrive.com>
2025-10-04 13:43:35 +0200

    init project

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -16,7 +16,7 @@ export const createSupabaseAnonClient = () => {
 export const createSupabaseServerClient = () => {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
   const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
+  const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY! ||supabaseAnonKey
   
   return createClient(supabaseUrl, serviceRoleKey, {
     auth: {
